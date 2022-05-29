9c90eb57d5032fbb42cf23044a4f7c0cdd172dab
Parsec <sailcali@gmail.com>
2022-05-28 19:07:56 -0700

    Moved to JDK11

--- src/main/java/com/kiowalabs/bankingbackend/BankingBackendApplication.java
+++ src/main/java/com/kiowalabs/bankingbackend/BankingBackendApplication.java
@@ -2,11 +2,16 @@ package com.kiowalabs.bankingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class BankingBackendApplication {
-
+public class BankingBackendApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BankingBackendApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BankingBackendApplication.class, args);
 	}
